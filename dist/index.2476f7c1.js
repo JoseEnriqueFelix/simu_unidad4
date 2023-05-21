ed5ec46e41d34377cdae6b1687a5b5b76b905d6e
Jose Felix <joseenrfelixesp@gmail.com>
2023-05-21 11:31:02 -0700

    algoritmo prueba de las series

--- dist/index.2476f7c1.js
+++ dist/index.2476f7c1.js
@@ -565,6 +565,18 @@ function generaTablaAleatoria(n) {
     for(let i = 0; i < n; i++)randoms.push(Math.random());
     return randoms;
 }
+function imprimeTablaAleatoria(valoresAleatorios) {
+    let tablaHTML = "<table><tr>Valores Aleatorios</tr>";
+    tablaHTML += "<tr><th>i</th><th>Valor aleatorio</th>";
+    for(let i = 0; i < valoresAleatorios.length; i++){
+        tablaHTML += "<tr>";
+        tablaHTML += "<th>" + (i + 1) + "</th>";
+        tablaHTML += "<th>" + valoresAleatorios[i] + "</th>";
+        tablaHTML += "</tr>";
+    }
+    tablaHTML += "</table>";
+    return tablaHTML;
+}
 function generaIntervalos(arr, w, k) {
     let inter = [];
     inter.push(Math.min(...arr) + w);
@@ -585,89 +597,129 @@ function chiCuadrada(intervalos, randoms, k, e) {
     o[a + 1] = suma;
     return o;
 }
-(async function() {
-    let precision = 2;
-    let n = prompt("Ingrese el valor de n");
-    if (!n) {
-        n = prompt("Ingrese el valor de n");
-        localStorage.setItem("n", n);
+function pruebaDeLasSeries(v, pares) {
+    let valoresO1 = [];
+    for(let i = 0; i < v; i++){
+        valoresO1[i] = [];
+        for(let j = 0; j < v; j++)valoresO1[i][j] = 0;
     }
-    let k = parseInt(Math.sqrt(n));
-    let e = n / k;
-    let valoresAleatorios = generaTablaAleatoria(n);
-    // Obtén una referencia al elemento contenedor de la tabla
-    let contenedorTabla2 = document.getElementById("contenedor-tabla2");
-    // Crea una variable para almacenar el contenido HTML de la tabla
-    let tablaHTML2 = "<table>";
-    tablaHTML2 += "<tr><th>i</th><th>Valor aleatorio</th>";
-    for(let i = 0; i < valoresAleatorios.length; i++){
-        tablaHTML2 += "<tr>";
-        tablaHTML2 += "<th>" + (i + 1) + "</th>";
-        tablaHTML2 += "<th>" + valoresAleatorios[i] + "</th>";
-        tablaHTML2 += "</tr>";
-    }
-    tablaHTML2 += "</table>";
-    contenedorTabla2.innerHTML = tablaHTML2;
-    let r = Math.max(...valoresAleatorios) - Math.min(...valoresAleatorios);
-    let w = r / k;
-    let intervalos = [];
-    intervalos = generaIntervalos(valoresAleatorios, w, k);
-    window.valoresO = [];
-    valoresO = chiCuadrada(intervalos, valoresAleatorios, k, e);
-    // Obtén una referencia al elemento contenedor de la tabla
-    let contenedorTabla = document.getElementById("contenedor-tabla");
-    // Crea una variable para almacenar el contenido HTML de la tabla
-    let tablaHTML = "<table>";
-    // Encabezados de la tabla
-    tablaHTML += "<tr>";
-    tablaHTML += "<th>i</th>";
-    tablaHTML += "<th>O</th>";
-    tablaHTML += "<th>E</th>";
-    tablaHTML += "<th>(O-E)</th>";
-    tablaHTML += "<th>((O-E)^2)/E</th>";
-    tablaHTML += "</tr>";
-    // Contenido de la tabla
-    for(var i = 0; i < valoresO.length - 1; i++){
-        tablaHTML += "<tr>";
-        tablaHTML += "<td>" + (i + 1) + "</td>";
-        tablaHTML += "<td>" + valoresO[i] + "</td>";
-        tablaHTML += "<td>" + e.toFixed(precision) + "</td>";
-        tablaHTML += "<td>" + (valoresO[i] - e).toFixed(precision) + "</td>";
-        tablaHTML += "<td>" + (Math.pow(valoresO[i] - e, 2) / e).toFixed(precision) + "</td>";
-        tablaHTML += "</tr>";
-    }
-    // Cierra la etiqueta de la tabla
-    tablaHTML += "</table>";
-    // Asigna el contenido HTML de la tabla al contenedor
-    contenedorTabla.innerHTML = tablaHTML;
-    let contenedor_h2 = document.getElementById("contenedor-h2");
-    let h2 = "<h2> Sumatoria = " + valoresO[valoresO.length - 1] + "</h2>";
-    contenedor_h2.innerHTML = h2;
-    let data = [];
-    for(let i = 0; i < valoresO.length - 1; i++){
-        let obj = {
-            iteracion: i + 1,
-            valorO: valoresO[i],
-            valorE: e
-        };
-        data.push(obj);
-    }
-    new (0, _autoDefault.default)(document.getElementById("grafica"), {
-        type: "bar",
-        data: {
-            labels: data.map((row)=>row.iteracion),
-            datasets: [
-                {
-                    label: "Num de o's obtenidad",
-                    data: data.map((row)=>row.valorO)
-                },
-                {
-                    label: "Valor esperado",
-                    data: data.map((row)=>row.valorE)
-                }
-            ]
+    let incremento = 1 / v;
+    let evaluacion1 = 0;
+    let evaluacion2 = 1;
+    for(let i = 0; i < v; i++){
+        for(let j = 0; j < v; j++){
+            for(let k = 0; k < pares.length; k++)if (pares[k].par1 > evaluacion1 && pares[k].par1 < evaluacion1 + incremento && pares[k].par2 < evaluacion2 && pares[k].par2 > evaluacion2 - incremento) valoresO1[i][j]++;
+            evaluacion1 += incremento;
         }
-    });
+        evaluacion2 -= incremento;
+        evaluacion1 = 0;
+    }
+    return valoresO1;
+}
+(async function() {
+    let precision = 4;
+    let menu = prompt("Ingrese: 1) Chi cuadrada; 3) Prueba de las series");
+    if (!menu) {
+        menu = prompt("Ingrese: 1) Chi cuadrada; 3) Prueba de las series");
+        localStorage.setItem("menu", menu);
+    }
+    let n = prompt("Ingrese el valor de n");
+    let v = prompt("ingrese los grados de libertad");
+    let valoresAleatorios = generaTablaAleatoria(n);
+    if (menu == 1) {
+        let k = parseInt(Math.sqrt(n));
+        let e = n / k;
+        let contenedorTablaAleatoria = document.getElementById("contenedor-tablaAleatoria");
+        contenedorTablaAleatoria.innerHTML = imprimeTablaAleatoria(valoresAleatorios);
+        let r = Math.max(...valoresAleatorios) - Math.min(...valoresAleatorios);
+        let w = r / k;
+        let intervalos = [];
+        intervalos = generaIntervalos(valoresAleatorios, w, k);
+        window.valoresO = [];
+        valoresO = chiCuadrada(intervalos, valoresAleatorios, k, e);
+        // Obtén una referencia al elemento contenedor de la tabla
+        let contenedorTablaChiCuadrada = document.getElementById("contenedor-tablaChiCuadrada");
+        // Crea una variable para almacenar el contenido HTML de la tabla
+        let tablaHTML = "<table><tr>Tabla chi cuadrada</tr>";
+        // Encabezados de la tabla
+        tablaHTML += "<tr>";
+        tablaHTML += "<th>i</th>";
+        tablaHTML += "<th>O</th>";
+        tablaHTML += "<th>E</th>";
+        tablaHTML += "<th>(O-E)</th>";
+        tablaHTML += "<th>((O-E)^2)/E</th>";
+        tablaHTML += "</tr>";
+        // Contenido de la tabla
+        for(var i = 0; i < valoresO.length - 1; i++){
+            tablaHTML += "<tr>";
+            tablaHTML += "<td>" + (i + 1) + "</td>";
+            tablaHTML += "<td>" + valoresO[i] + "</td>";
+            tablaHTML += "<td>" + e.toFixed(precision) + "</td>";
+            tablaHTML += "<td>" + (valoresO[i] - e).toFixed(precision) + "</td>";
+            tablaHTML += "<td>" + (Math.pow(valoresO[i] - e, 2) / e).toFixed(precision) + "</td>";
+            tablaHTML += "</tr>";
+        }
+        // Cierra la etiqueta de la tabla
+        tablaHTML += "</table>";
+        // Asigna el contenido HTML de la tabla al contenedor
+        contenedorTablaChiCuadrada.innerHTML = tablaHTML;
+        let contenedorSumatoria = document.getElementById("contenedor-sumatoria");
+        let sumatoria = "<h2> Sumatoria = " + valoresO[valoresO.length - 1] + "</h2>";
+        contenedorSumatoria.innerHTML = sumatoria;
+        let data = [];
+        for(let i = 0; i < valoresO.length - 1; i++){
+            let obj = {
+                iteracion: i + 1,
+                valorO: valoresO[i],
+                valorE: e
+            };
+            data.push(obj);
+        }
+        new (0, _autoDefault.default)(document.getElementById("grafica"), {
+            type: "bar",
+            data: {
+                labels: data.map((row)=>row.iteracion),
+                datasets: [
+                    {
+                        label: "Num de o's obtenidad",
+                        data: data.map((row)=>row.valorO)
+                    },
+                    {
+                        label: "Valor esperado",
+                        data: data.map((row)=>row.valorE)
+                    }
+                ]
+            }
+        });
+    } else if (menu == 2) ;
+    else if (menu == 3) {
+        //let celdas=v*v
+        let valoresAleatorios = generaTablaAleatoria(n);
+        let contenedorTablaAleatoria = document.getElementById("contenedor-tablaAleatoria");
+        contenedorTablaAleatoria.innerHTML = imprimeTablaAleatoria(valoresAleatorios);
+        let pares = [];
+        for(let i = 0; i < valoresAleatorios.length; i++)if (i == valoresAleatorios.length - 1) {
+            let obj = {
+                par1: valoresAleatorios[i],
+                par2: valoresAleatorios[i]
+            };
+            pares.push(obj);
+        } else {
+            let obj = {
+                par1: valoresAleatorios[i],
+                par2: valoresAleatorios[i + 1]
+            };
+            pares.push(obj);
+        }
+        let contenedorTablaPruebaDeLasSeries = document.getElementById("contenedor-tablaPruebaDeLasSeries");
+        // Crea una variable para almacenar el contenido HTML de la tabla
+        let tablaHTML = "<table><tr>Pares ordenados</tr><tr><th>i</th><th>Numero 1</th><th>Numero 2</th></tr>";
+        for(let i = 0; i < valoresAleatorios.length; i++)tablaHTML += "<tr><td>" + (i + 1) + "</td><td>" + pares[i].par1 + "</td><td>" + pares[i].par2 + "</td></tr>";
+        tablaHTML += "</table>";
+        contenedorTablaPruebaDeLasSeries.innerHTML = tablaHTML;
+        let valoresO1 = pruebaDeLasSeries(v, pares);
+        console.log(valoresO1);
+    }
 })();
 
 },{"chart.js/auto":"d8NN9","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"d8NN9":[function(require,module,exports) {
--- source/pruebasEstadisticas.js
+++ source/pruebasEstadisticas.js
@@ -8,6 +8,19 @@ function generaTablaAleatoria(n){
     return randoms
 }
 
+function imprimeTablaAleatoria(valoresAleatorios){
+  let tablaHTML = "<table><tr>Valores Aleatorios</tr>";
+  tablaHTML += "<tr><th>i</th><th>Valor aleatorio</th>"
+  for(let i=0; i<valoresAleatorios.length; i++){
+    tablaHTML += "<tr>"
+    tablaHTML += "<th>" + (i+1) + "</th>"
+    tablaHTML += "<th>" + valoresAleatorios[i] + "</th>"
+    tablaHTML += "</tr>"
+  }
+  tablaHTML += "</table>"
+  return tablaHTML
+}
+
 function generaIntervalos(arr, w, k){
     let inter=[]
     inter.push(Math.min(...arr) + w)
@@ -40,99 +53,152 @@ function chiCuadrada(intervalos, randoms, k, e){
     return o
 }
 
+function pruebaDeLasSeries(v, pares){
+  let valoresO=[]
+  for(let i=0; i<v; i++){
+    valoresO[i]=[]
+    for(let j=0; j<v; j++){
+      valoresO[i][j]=0
+    }
+  }
+  let incremento=1/v
+  let evaluacion1=0
+  let evaluacion2=1
+  for(let i=0; i<v; i++){
+    for (let j=0; j<v; j++){
+      for(let k=0; k<pares.length; k++){
+        if(pares[k].par1>evaluacion1 && pares[k].par1<(evaluacion1+incremento) && pares[k].par2<evaluacion2 && pares[k].par2>(evaluacion2-incremento)){
+          valoresO[i][j]++;
+        }
+      }
+      evaluacion1+=incremento
+    }
+    evaluacion2-=incremento
+    evaluacion1=0
+  }
+  return valoresO
+}
+
 
 (async function() {
-  let precision=2
+  let precision=4
+  let menu = prompt("Ingrese: 1) Chi cuadrada; 3) Prueba de las series");
+
+  if (!menu) {
+    menu = prompt("Ingrese: 1) Chi cuadrada; 3) Prueba de las series");
+    localStorage.setItem('menu', menu);
+  }
   let n=prompt("Ingrese el valor de n")
-  if (!n) {
-    n = prompt("Ingrese el valor de n");
-    localStorage.setItem('n', n);
-  }
-
-  let k=parseInt(Math.sqrt(n))
-  let e=n/k
+  let v=prompt("ingrese los grados de libertad")
   let valoresAleatorios=generaTablaAleatoria(n)
-  // Obtén una referencia al elemento contenedor de la tabla
-  let contenedorTabla2 = document.getElementById("contenedor-tabla2");
+  if(menu==1){
+    let k=parseInt(Math.sqrt(n))
+    let e=n/k
+    let contenedorTablaAleatoria = document.getElementById("contenedor-tablaAleatoria");
+    contenedorTablaAleatoria.innerHTML=imprimeTablaAleatoria(valoresAleatorios)
+    let r=Math.max(...valoresAleatorios) - Math.min(...valoresAleatorios)
+    let w=r/k
+    let intervalos = []
+    intervalos=generaIntervalos(valoresAleatorios, w, k)
+    window.valoresO=[]
+    valoresO=chiCuadrada(intervalos, valoresAleatorios, k, e)
+    // Obtén una referencia al elemento contenedor de la tabla
+    let contenedorTablaChiCuadrada = document.getElementById("contenedor-tablaChiCuadrada");
 
-  // Crea una variable para almacenar el contenido HTML de la tabla
-  let tablaHTML2 = "<table>";
-  tablaHTML2 += "<tr><th>i</th><th>Valor aleatorio</th>"
-  for(let i=0; i<valoresAleatorios.length; i++){
-    tablaHTML2 += "<tr>"
-    tablaHTML2 += "<th>" + (i+1) + "</th>"
-    tablaHTML2 += "<th>" + valoresAleatorios[i] + "</th>"
-    tablaHTML2 += "</tr>"
-  }
-  tablaHTML2 += "</table>"
+    // Crea una variable para almacenar el contenido HTML de la tabla
+    let tablaHTML = "<table><tr>Tabla chi cuadrada</tr>";
 
-  contenedorTabla2.innerHTML=tablaHTML2
-
-  let r=Math.max(...valoresAleatorios) - Math.min(...valoresAleatorios)
-  let w=r/k
-  let intervalos = []
-  intervalos=generaIntervalos(valoresAleatorios, w, k)
-  window.valoresO=[]
-  valoresO=chiCuadrada(intervalos, valoresAleatorios, k, e)
-  // Obtén una referencia al elemento contenedor de la tabla
-  let contenedorTabla = document.getElementById("contenedor-tabla");
-
-  // Crea una variable para almacenar el contenido HTML de la tabla
-  let tablaHTML = "<table>";
-
-  // Encabezados de la tabla
-  tablaHTML += "<tr>";
-  tablaHTML += "<th>i</th>";
-  tablaHTML += "<th>O</th>";
-  tablaHTML += "<th>E</th>";
-  tablaHTML += "<th>(O-E)</th>";
-  tablaHTML += "<th>((O-E)^2)/E</th>";
-  tablaHTML += "</tr>";
-
-  // Contenido de la tabla
-  for (var i = 0; i < valoresO.length - 1; i++) {
+    // Encabezados de la tabla
     tablaHTML += "<tr>";
-    tablaHTML += "<td>" + (i + 1) + "</td>";
-    tablaHTML += "<td>" + valoresO[i] + "</td>";
-    tablaHTML += "<td>" + e.toFixed(precision) + "</td>";
-    tablaHTML += "<td>" + (valoresO[i] - e).toFixed(precision) + "</td>";
-    tablaHTML += "<td>" + (Math.pow((valoresO[i] - e), 2) / e).toFixed(precision) + "</td>";
+    tablaHTML += "<th>i</th>";
+    tablaHTML += "<th>O</th>";
+    tablaHTML += "<th>E</th>";
+    tablaHTML += "<th>(O-E)</th>";
+    tablaHTML += "<th>((O-E)^2)/E</th>";
     tablaHTML += "</tr>";
+
+    // Contenido de la tabla
+    for (var i = 0; i < valoresO.length - 1; i++) {
+      tablaHTML += "<tr>";
+      tablaHTML += "<td>" + (i + 1) + "</td>";
+      tablaHTML += "<td>" + valoresO[i] + "</td>";
+      tablaHTML += "<td>" + e.toFixed(precision) + "</td>";
+      tablaHTML += "<td>" + (valoresO[i] - e).toFixed(precision) + "</td>";
+      tablaHTML += "<td>" + (Math.pow((valoresO[i] - e), 2) / e).toFixed(precision) + "</td>";
+      tablaHTML += "</tr>";
+    }
+
+    // Cierra la etiqueta de la tabla
+    tablaHTML += "</table>";
+
+    // Asigna el contenido HTML de la tabla al contenedor
+    contenedorTablaChiCuadrada.innerHTML = tablaHTML;
+    let contenedorSumatoria=document.getElementById("contenedor-sumatoria");
+    
+    let sumatoria="<h2> Sumatoria = " + valoresO[valoresO.length-1] + "</h2>"
+    contenedorSumatoria.innerHTML=sumatoria
+
+    let data = []
+
+    for (let i = 0; i < valoresO.length-1; i++) {
+      let obj = { iteracion: i+1, valorO: valoresO[i], valorE: e}
+      data.push(obj)
+    }
+
+    new Chart(
+      document.getElementById('grafica'),
+      {
+        type: 'bar',
+        data: {
+          labels: data.map(row => row.iteracion),
+          datasets: [
+            {
+              label: "Num de o's obtenidad",
+              data: data.map(row => row.valorO)
+            },
+            {
+              label: "Valor esperado",
+              data: data.map(row => row.valorE)
+            }
+          ]
+        }
+      }
+    );
   }
+  else if(menu==2){
 
-  // Cierra la etiqueta de la tabla
-  tablaHTML += "</table>";
-
-  // Asigna el contenido HTML de la tabla al contenedor
-  contenedorTabla.innerHTML = tablaHTML;
-  let contenedor_h2=document.getElementById("contenedor-h2");
-  let h2="<h2> Sumatoria = " + valoresO[valoresO.length-1] + "</h2>"
-  contenedor_h2.innerHTML=h2
-
-  let data = []
-
-  for (let i = 0; i < valoresO.length-1; i++) {
-    let obj = { iteracion: i+1, valorO: valoresO[i], valorE: e}
-    data.push(obj)
   }
-
-  new Chart(
-    document.getElementById('grafica'),
-    {
-      type: 'bar',
-      data: {
-        labels: data.map(row => row.iteracion),
-        datasets: [
-          {
-            label: "Num de o's obtenidad",
-            data: data.map(row => row.valorO)
-          },
-          {
-            label: "Valor esperado",
-            data: data.map(row => row.valorE)
-          }
-        ]
+  else if(menu==3){
+    //let celdas=v*v
+    let valoresAleatorios=generaTablaAleatoria(n)
+    let contenedorTablaAleatoria = document.getElementById("contenedor-tablaAleatoria");
+    contenedorTablaAleatoria.innerHTML=imprimeTablaAleatoria(valoresAleatorios)
+    let pares=[]
+    for(let i=0; i<valoresAleatorios.length; i++){
+      if (i==valoresAleatorios.length-1){
+        let obj = { par1: valoresAleatorios[i], par2: valoresAleatorios[i]}
+        pares.push(obj)
+      }
+      else{
+        let obj = { par1: valoresAleatorios[i], par2: valoresAleatorios[i+1]}
+        pares.push(obj)
       }
     }
-  );
+    let contenedorTablaPruebaDeLasSeries = document.getElementById("contenedor-tablaPruebaDeLasSeries");
+
+    // Crea una variable para almacenar el contenido HTML de la tabla
+    let tablaHTML = "<table><tr>Pares ordenados</tr><tr><th>i</th><th>Numero 1</th><th>Numero 2</th></tr>"
+    for(let i=0; i<valoresAleatorios.length; i++){
+      tablaHTML += "<tr><td>" + (i+1) + "</td><td>" + pares[i].par1 + "</td><td>" + pares[i].par2 +"</td></tr>"
+    }
+
+    tablaHTML += "</table>"
+    contenedorTablaPruebaDeLasSeries.innerHTML=tablaHTML
+
+    let valoresO=pruebaDeLasSeries(v, pares)
+    console.log(valoresO)
+
+
+
+  }
 })();
